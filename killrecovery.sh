44a29e50b9603a9b555a329f566ad159fdc3e707
vurrut <calvoredondo@gmail.com>
2012-06-25 16:17:25 +0200

    set correct killrecovery
    Change-Id: I98fe86a324c94f284f23c193593d90c1074a8b4c

--- killrecovery.sh
+++ killrecovery.sh
@@ -6,17 +6,11 @@ touch /tmp/.ignorebootmessage
 kill $(ps | grep /sbin/adbd)
 kill $(ps | grep /sbin/recovery)
 
-# On the Galaxy S, the recovery comes test signed, but the
+# On the pascal2 the recovery comes test signed, but the
 # recovery is not automatically restarted.
-if [ -f /init.smdkc110.rc ]
+if [ -f /init.rk29board.rc ]
 then
     /sbin/recovery &
 fi
 
-# Droid X
-if [ -f /init.mapphone_cdma.rc ]
-then
-    /sbin/recovery &
-fi
 
-exit 1
